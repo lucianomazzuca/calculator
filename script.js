fadb7649b4689b67cbaf766c457236ab23657ea7
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2020-08-06 00:42:38 -0300

    Minimal functioning

--- script.js
+++ script.js
@@ -36,12 +36,33 @@ function operate(num1, num2, operator){
 
 //Get input
 
-    const btnNumber = document.querySelectorAll('.number');
-    let operand;
+    const buttons = document.querySelectorAll('button')
+    const display = document.querySelector('.display')
+    let firstOperand = '';
+    let secondOperand = '';
+    let operator = '';
 
-    btnNumber.forEach(btn => {
+
+    buttons.forEach(btn => {
         btn.addEventListener('click', function(e){
-            console.log(btn.innerHTML);
+            if(operator == '' && this.className == 'number'){
+                firstOperand += this.innerHTML;
+                display.innerText = firstOperand;
+                console.log(firstOperand)
+            }   
+            else if(operator == '' && this.className == 'operator'){
+                operator = this.innerHTML;
+                console.log(operator);
+            }
+            else if(operator != '' && this.className == 'number'){
+                secondOperand += this.innerHTML;
+                display.innerText = secondOperand;
+                console.log(secondOperand);
+            }
+            else if(this.id == 'result'){
+                display.innerText = operate(Number(firstOperand), Number(secondOperand), operator)
+            }
         })
     })
+    
 
